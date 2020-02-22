8f5de0059e2e2acd4f34a5fbe430fe5511f90c6d
pyavchik <pyavchik@gmail.com>
2020-02-22 11:16:07 +0200

    added getPhoto method

--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPhoto() {
+    return this.http.get('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: 'Client-ID N0jdAqfZ04nCph89nf-LilWPPmWVk7-GkMAy-ZEvBtc'
+      }
+    });
+  }
 }
