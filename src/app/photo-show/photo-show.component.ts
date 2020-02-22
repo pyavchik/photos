9be30b7c426790f33796667ae5568dc277b60746
pyavchik <pyavchik@gmail.com>
2020-02-22 12:06:55 +0200

    isplaying the image added

--- src/app/photo-show/photo-show.component.ts
+++ src/app/photo-show/photo-show.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PhotosService} from '../photos.service';
 
 @Component({
   selector: 'app-photo-show',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-show.component.css']
 })
 export class PhotoShowComponent implements OnInit {
+  photoUrl: string;
 
-  constructor() { }
+  constructor(private photoService: PhotosService) {
+    this.photoService.getPhoto().subscribe((response) => {
+      this.photoUrl = response.urls.regular;
+    });
+  }
 
   ngOnInit() {
   }
--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
+interface UnsplashResponse {
+  urls: {
+    regular: string,
+    full: string
+  };
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +16,7 @@ export class PhotosService {
   constructor(private http: HttpClient) { }
 
   getPhoto() {
-    return this.http.get('https://api.unsplash.com/photos/random', {
+    return this.http.get<UnsplashResponse>('https://api.unsplash.com/photos/random', {
       headers: {
         Authorization: 'Client-ID N0jdAqfZ04nCph89nf-LilWPPmWVk7-GkMAy-ZEvBtc'
       }
